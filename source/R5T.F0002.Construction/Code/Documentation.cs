df586adb05a0169f234199db01bc8b7f8ff9541b
davidcoats <david@getsafetycone.com>
2022-05-03 13:24:40 -0700

    Initial commit of source-stub.

---
+++ source/R5T.F0002.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0002.Construction
+{
+    /// <summary>
+    /// Construction project for Common operations on System namespace, and related, types, like Predicate, regular expression pattern, Index etc..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.F0002/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0002
+{
+    /// <summary>
+    /// Common operations on System namespace, and related, types, like Predicate, regular expression pattern, Index etc.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
