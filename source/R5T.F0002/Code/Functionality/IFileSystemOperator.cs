f3bd0e3017c874370917898cb764b633e3e43cc4
davidcoats <david@getsafetycone.com>
2024-05-30 22:22:28 -0700

    Interim changes.

--- source/R5T.F0002/Code/Functionality/IFileSystemOperator.cs
+++ source/R5T.F0002/Code/Functionality/IFileSystemOperator.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 
 using R5T.T0132;
+using R5T.T0221;
 
 
 namespace R5T.F0002
@@ -9,8 +11,20 @@ namespace R5T.F0002
 	public partial interface IFileSystemOperator : IFunctionalityMarker,
         F0000.IFileSystemOperator,
         L0071.IFileSystemOperator
-        
 	{
-        
+        public void Copy_File(FileCopyPair pair)
+        {
+            this.Copy_File(
+                pair.SourceFilePath,
+                pair.DestinationFilePath);
+        }
+
+        public void Copy_Files(IEnumerable<FileCopyPair> pairs)
+        {
+            foreach (var pair in pairs)
+            {
+                this.Copy_File(pair);
+            }
+        }
     }
 }
